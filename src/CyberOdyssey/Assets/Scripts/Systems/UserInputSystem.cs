e9c91ba7119fcf94e1e69f12b7e06cf8bef075ef
Oleksii Sachek <alexeycacheknew@gmail.com>
2019-10-04 00:31:47 +0300

    Initial commit

---
+++ src/CyberOdyssey/Assets/Scripts/Systems/UserInputSystem.cs
@@ -0,0 +1,12 @@
+﻿namespace CyberOdyssey.Systems
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using Unity.Entities;
+    using UnityEngine;
+
+    public sealed class UserInputSystem : ComponentSystem
+    {
+        
+    }
+}
