f49c04da5663d98504ea85b0d476826d44133872
Oleksii Sachek <alexeycacheknew@gmail.com>
2019-12-29 17:19:48 +0200

    Update 1

---
+++ src/CyberOdyssey/Assets/Scripts/ProjectileMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class ProjectileMovement : MonoBehaviour
+{
+    public float Speed;
+
+    public int Damage;
+
+    public Vector2 Velocity;
+
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector2(Velocity.x * Speed, Velocity.y * Speed);
+
+        if (transform.position.magnitude > 200f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        var spacesheep = other.GetComponent<Spacesheep>();
+        if (spacesheep != null)
+        {
+            spacesheep.Damage(Damage);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ src/CyberOdyssey/Assets/Scripts/Spacesheep.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spacesheep : MonoBehaviour
+{
+    public int Health;
+
+    public void Damage(int damage)
+    {
+        Health -= damage;
+        Debug.Log($"Spacesheep damaged by: {damage}. Health left: {Health}");
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
